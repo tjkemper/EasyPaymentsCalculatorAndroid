2bf693a81c0d828252e8bcd92ebbc6e3a4b23542
Taylor Kemper <tjkemper6@gmail.com>
2016-07-17 18:49:45 -0400

    initial PayCal Android Project commit

---
+++ PayCal/app/src/main/java/io/kemper/paycal/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package io.kemper.paycal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
